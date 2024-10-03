bcde6bcda31f7e32114a86d8bb9f509a5fc9257b
tangxueni <tang.xuen@northeastern.edu>
2024-10-03 19:39:01 -0400

    a2 CSS and Bootstrap

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -32,7 +32,7 @@ export default function TOC() {
           My GitHub
         </a> </li>
       <li className="nav-item">
-        <a id="wd-github" href="https://github.com/Xueni-T/kanbas-react-web-app" className="nav-link">
+        <a id="wd-github" href="https://github.com/Xueni-T/kanbas-react-web-app/tree/a2" className="nav-link">
           My Repository
         </a> </li>
     </ul>
